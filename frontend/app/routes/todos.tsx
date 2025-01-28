528419af5370db95903984d9c5b8d520a101eaf3
Shkarlat Dmitriy <shkarlat.dmitry@yandex.ru>
2025-01-28 00:23:38 -0800

    BUG 🧨 : something goes wrong with indeterminate

--- frontend/app/routes/todos.tsx
+++ frontend/app/routes/todos.tsx
@@ -19,13 +19,16 @@ export async function clientLoader() {
 
 clientLoader.hydrate = true as const;
 
-function arrayFromCompleted(arr: ITodo[]) {
+// list of completed ==> [false,false,true,false,true]
+function arrayFromCompleted(arr: ITodo[]): boolean[] {
   return Array.from(arr, (x) => x.completed);
 }
 
 function Todos({ loaderData }: Route.ComponentProps) {
   const [todos, setTodos] = useState(loaderData);
-  const [totalChecked, setTotalChecked] = useState(loaderData.length);
+  const [totalChecked, setTotalChecked] = useState(
+    arrayFromCompleted(loaderData).filter(Boolean).length
+  );
   const [isCheckedAll, setIsCheckedAll] = useState(false);
   const [checkedState, setCheckedState] = useState(
     arrayFromCompleted(loaderData)
@@ -34,58 +37,97 @@ function Todos({ loaderData }: Route.ComponentProps) {
   const indeterminateRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
+    // 🧨 DEBUG
+    // TypeError: Cannot set properties of null (setting 'indeterminate')
     indeterminateRef.current!.indeterminate =
-      !isCheckedAll && checkedState.some(Boolean);
+      todos.length && !isCheckedAll && checkedState.some(Boolean);
   }, [isCheckedAll, checkedState.some(Boolean)]);
 
-  useEffect(() => {
-    setTotalChecked(arrayFromCompleted(todos).filter(Boolean).length);
-  }, [todos.length]);
+  // useEffect(() => {
+  //   setTotalChecked(arrayFromCompleted(todos).filter(Boolean).length);
+  // }, [todos.length]);
 
   const onToggleHandler = (position: number) => {
     const updatedCheckedState = checkedState.map((item, index) =>
       index === position ? !item : item
     );
 
-    if (
-      todos.length !== updatedCheckedState.filter(Boolean).length ||
-      updatedCheckedState.length === 0
-    ) {
+    setTotalChecked(updatedCheckedState.filter(Boolean).length);
+
+    // 🧨 DEBUG: This is not working as expected.
+
+    // Example: 1.
+    // if (checkedState.filter(Boolean).length === 0) {
+    //   setIsCheckedAll(false);
+    //   indeterminateRef.current!.indeterminate = false;
+    // } else if (totalChecked === todos.length) {
+    //   setIsCheckedAll(true);
+    //   indeterminateRef.current!.indeterminate = false;
+    // } else {
+    //   setIsCheckedAll(false);
+    //   indeterminateRef.current!.indeterminate = false;
+    // }
+
+    // Example: 2.
+    // if (
+    //   todos.length !== updatedCheckedState.filter(Boolean).length ||
+    //   updatedCheckedState.length === 0 ||
+    //   todos.length === 0
+    // ) {
+    //   setIsCheckedAll(false);
+    // } else {
+    //   setIsCheckedAll(true);
+    // }
+
+    // Example: 3.
+    debugger;
+    if (totalChecked === 0) {
       setIsCheckedAll(false);
-    } else {
+      indeterminateRef.current!.indeterminate = false;
+    } else if (totalChecked === todos.length) {
       setIsCheckedAll(true);
+      indeterminateRef.current!.indeterminate = false;
+    } else {
+      setIsCheckedAll(false);
+      indeterminateRef.current!.indeterminate = true;
     }
 
-    setTotalChecked(updatedCheckedState.filter(Boolean).length);
+    // setTotalChecked(updatedCheckedState.filter(Boolean).length);
     setCheckedState(updatedCheckedState);
+    setTodos(
+      updatedCheckedState.map((completed, index) => ({
+        ...todos[index],
+        completed,
+      }))
+    );
   };
 
   const onSelectAllHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIsCheckedAll(!isCheckedAll);
     setCheckedState(new Array(todos.length).fill(!isCheckedAll));
     setTotalChecked(!isCheckedAll ? todos.length : 0);
+    setTodos(
+      todos.map((todo: ITodo) => ({ ...todo, completed: !isCheckedAll }))
+    );
   };
 
-  const onRemoveHandler = useCallback(
-    (idx: number, _id: string) => () => {
-      const updatedTodos = todos.filter(
-        (_: ITodo, index: number) => index !== idx
-      );
-      setCheckedState(updatedTodos);
-      setTotalChecked(updatedTodos.length);
-    },
-    [todos.length]
-  );
+  const onRemoveHandler = (idx: number, _id: string) => () => {
+    const updatedTodos = todos.filter(
+      (_: ITodo, index: number) => index !== idx
+    );
+    setCheckedState(arrayFromCompleted(updatedTodos));
+    setTodos(updatedTodos);
+  };
 
   return (
-    <div className={`${todosStyles.todosPage}`}>
+    <div className={todosStyles.todosPage}>
       <form action=''>
         <fieldset>
           <legend>
             Choose your interests {totalChecked}/{todos.length}
           </legend>
           <ul>
-            {todos.map(({ _id, title, completed }: any, index: number) => {
+            {todos.map(({ _id, title, completed }: ITodo, index: number) => {
               return (
                 <li key={_id}>
                   <label>
@@ -105,20 +147,22 @@ function Todos({ loaderData }: Route.ComponentProps) {
               );
             })}
           </ul>
-          <div>
-            <label>
-              <input
-                ref={indeterminateRef}
-                className='select-all'
-                type='checkbox'
-                name='Select all'
-                value='Select all'
-                checked={isCheckedAll}
-                onChange={(e) => onSelectAllHandler(e)}
-              />
-              <span>Select all</span>
-            </label>
-          </div>
+          {todos.length > 0 && (
+            <div>
+              <label>
+                <input
+                  ref={indeterminateRef}
+                  className='select-all'
+                  type='checkbox'
+                  name='Select all'
+                  value='Select all'
+                  checked={isCheckedAll}
+                  onChange={(e) => onSelectAllHandler(e)}
+                />
+                <span>Select all</span>
+              </label>
+            </div>
+          )}
         </fieldset>
       </form>
     </div>
