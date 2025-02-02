0483055e733b4dca03f7720da2a2e2f7d3479210
Shkarlat Dmitriy <shkarlat.dmitry@yandex.ru>
2025-02-01 21:31:06 -0800

    add: switch component

---
+++ frontend/app/ui/switch/index.tsx
@@ -0,0 +1,17 @@
+import classNames from 'classnames/bind';
+import styles from './switch.module.css';
+import type { SwitchProps } from './types';
+
+function Switch({ className, id }: SwitchProps) {
+  // ✅ bind the styles to classNames
+  const cx = classNames.bind(styles);
+  const classes = cx(styles.switchEl, className);
+  return (
+    <span className={classes}>
+      <input type='checkbox' id={id} />
+      <label htmlFor={id} className={styles.switch}></label>
+    </span>
+  );
+}
+
+export default Switch;
---
+++ frontend/app/ui/switch/types/index.ts
@@ -0,0 +1,4 @@
+export type SwitchProps = {
+  id: string;
+  className?: string;
+};
