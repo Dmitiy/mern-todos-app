3dd997980f89da11be3fe335f2cdec222a349865
Shkarlat Dmitriy <shkarlat.dmitry@yandex.ru>
2025-02-04 14:01:41 -0800

    add: container component

---
+++ frontend/app/ui/container/index.tsx
@@ -0,0 +1,13 @@
+import classNames from 'classnames';
+
+import styles from './container.module.css';
+import type { ContainerProps } from './types';
+
+function Container({ className, children }: ContainerProps) {
+  const cx = classNames.bind(styles);
+  const classes = cx(styles.section, className);
+
+  return <div className={classes}>{children}</div>;
+}
+
+export default Container;
