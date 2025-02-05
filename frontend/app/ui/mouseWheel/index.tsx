0edd610070b91d3a9b926b8db09852b7776b0da4
Shkarlat Dmitriy <shkarlat.dmitry@yandex.ru>
2025-02-04 17:59:53 -0800

    add: mouse wheel component

---
+++ frontend/app/ui/mouseWheel/index.tsx
@@ -0,0 +1,18 @@
+import classNames from 'classnames';
+import styles from './mouseWheel.module.css';
+import type { MouseWheelProps } from './types';
+
+function MouseWheel({ className }: MouseWheelProps) {
+  const cx = classNames.bind(styles);
+  const classes = cx(styles.mouseIcon, className);
+
+  return (
+    <ul className={classes}>
+      <li className={styles.wheel}></li>
+      <li className={styles.wheel}></li>
+      <li className={styles.wheel}></li>
+    </ul>
+  );
+}
+
+export default MouseWheel;
---
+++ frontend/app/ui/mouseWheel/types/index.ts
@@ -0,0 +1,3 @@
+export type MouseWheelProps = {
+  className?: string;
+};
