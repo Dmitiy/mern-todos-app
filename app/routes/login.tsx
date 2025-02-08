bbaea812058e005f4054a4dfaf46fbda2c5d3998
Dmitriy <shkarlat.dmitry@yandex.ru>
2025-02-09 02:01:48 +0300

    Draft vite config (#1)
    * draft
    
    * test version

---
+++ app/routes/login.tsx
@@ -0,0 +1,55 @@
+import Button from '@/ui/button';
+import Input from '@/ui/input';
+import { data, useFetcher } from 'react-router';
+import type { Route } from './+types/login';
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: 'Login' },
+    { name: 'description', content: 'Welcome to Login page!' },
+  ];
+}
+
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const email = String(formData.get('email'));
+  const password = String(formData.get('password'));
+
+  const errors: { email?: string; password?: string } = {};
+
+  if (!email.includes('@')) {
+    errors.email = 'Invalid email address';
+  }
+
+  if (password.length < 3) {
+    errors.password = 'Password should be at least 3 characters';
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return data({ errors }, { status: 400 });
+  }
+
+  // Redirect to home if validation is successful
+  // return redirect('/login');
+}
+
+function Login({}: Route.ComponentProps) {
+  let fetcher = useFetcher();
+  let errors = fetcher.data?.errors;
+  return (
+    <fetcher.Form method='post'>
+      <p>
+        <Input type='email' name='email' /> Email:
+        {errors?.email ? <em>{errors.email}</em> : null}
+      </p>
+      <p>
+        <Input type='password' name='password' /> Password:
+        {errors?.password ? <em>{errors.password}</em> : null}
+      </p>
+
+      <Button type='submit'>Sign Up</Button>
+    </fetcher.Form>
+  );
+}
+
+export default Login;
